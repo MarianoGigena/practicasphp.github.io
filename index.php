e35018ba39b7712cfe3b6069f035abf8d77c95a9
MarianoGigena <95063581+MarianoGigena@users.noreply.github.com>
2023-09-23 01:33:55 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Prueba 5</h1>
+    <?php
+
+        $agua=7;
+        $cafe=8;
+        $mensaje="Cafe terminado";
+        $suma=$agua+$cafe;
+        echo($mensaje." el resultado es =".$suma)
+
+    ?>
+</body>
+</html>
